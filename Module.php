0d78fd3cb2ecaae4534a1114ce1af70059cb5d5d
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2019-10-28 18:56:26 +0200

    Change default icon in nav

--- Module.php
+++ Module.php
@@ -75,7 +75,7 @@ class Module extends BaseModule
         $items = [
             'label' => $this->name,
             'url' => [$this->routePrefix . '/'. $this->id],
-            'icon' => 'fa-tasks',
+            'icon' => 'fa-address-card',
             'active' => in_array(\Yii::$app->controller->module->id, [$this->id])
         ];
         return $items;
